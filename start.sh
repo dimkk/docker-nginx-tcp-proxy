b41473bda11395ba58d4e0b8e4555bbe143b755d
Volkovsky Dima <dimkk@outlook.com>
2017-03-19 15:03:11 +0300

    script fix

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-if [-a $OVPN]; then
-  exec openvpn --config $OVPN
-  sleep 10
+
+echo 'checking $OVPN'
+if [ -a $OVPN ]
+  then
+    echo "got ovpn, trying to connect"
+    exec openvpn --config $OVPN
+    sleep 10
 fi
+
+echo 'starting nginx...'
 exec /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
